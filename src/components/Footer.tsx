1b5cdf0dca3e2999aa332e708737b646b88d0d22
teetri <tee@teemidnight.local>
2022-10-21 23:35:38 +0700

    Added graphic assets and Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image"
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        <div className="bg-cuof-grey-01 py-8 text-[13px]">
+          <div className="flex flex-col items-center space-y-8">
+            <div className="flex flex-row justify-between items-center w-[320px]">
+              <div className="w-1/2">
+                <Image src="/graphic/cuof-logo-text.svg" width="297" height="98" alt="" />
+              </div>
+              <a href="https://www.github.com/chula-overflow" target="_blank">
+                <div className="flex flex-row mr-2">
+                  <Image src="/graphic/github-icon.svg" width="18" height="18" alt="" />
+                  <p className="font-medium pl-1">/chula-overflow</p>
+                </div>
+              </a>
+            </div>
+            <div className="flex flex-col items-centers space-y-1">
+              <div className="flex justify-center items-center">
+                <p className="font-medium">Created by</p>
+              </div>
+              <div className="flex flex-row items-center  justify-center space-x-4">
+                <div>
+                  <p>Panithi Makthiengtrong</p>
+                  <p>Pannawich Lohanimit</p>
+                  <p>Purich Nuamchit</p>
+                </div>
+                <div>
+                  <p>Saharat Nuamchit</p>
+                  <p>Tanadon Santisan</p>
+                  <p>Thanarit Trimaharoek</p>
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-center items-center">
+              <p>Copyright &copy; 2022, All rights reserved</p>
+            </div>
+          </div>
+        </div>
+        <div className="w-full h-4 bg-cuof-gradient-h"></div>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,19 @@
+import Footer from "@components/Footer"
 import { DescribeRoute } from "@components/Meta/DescribeRoute"
 import type { NextPage } from "next"
 
 const Home: NextPage = () => {
   return (
     <DescribeRoute title="Next.js boilerplate" description="by betich">
-      <main>
-        <h1>Next.js App</h1>
-        <p>Chula Overflow</p>
-        <p>จุฬาโอเวอร์โฟล์ว</p>
-        <p className="font-semibold">Chula Overflow </p>
-        <div className="w-full h-6 bg-cuof-gradient-h"></div>
-      </main>
+      <div>
+        <main>
+          <h1>Next.js App</h1>
+          <p>Chula Overflow</p>
+          <p>จุฬาโอเวอร์โฟล์ว</p>
+          <p className="font-semibold">Chula Overflow </p>
+        </main>
+        <Footer />
+      </div>
     </DescribeRoute>
   )
 }
