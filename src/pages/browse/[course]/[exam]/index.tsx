35d425ec657796f4a44100e8a00405a9393c43d8
tntons <saharat.nuamchit@gmail.com>
2022-10-24 04:28:09 +0700

    create browse page

---
+++ src/pages/browse/[course]/[exam]/index.tsx
@@ -0,0 +1,88 @@
+import Footer from "@components/Footer"
+import React, { useEffect, useState } from "react"
+import { useRouter } from "next/router"
+import Link from "next/link";
+import Image from "next/image"
+
+const problem = [
+  {
+    question:"Find margin of error",
+    detail:"A sphere is measured to have a radius of 10m with an error of ..."
+  },
+  {
+    question:"Find margin of error",
+    detail:"A sphere is measured to have a radius of 10m with an error of ..."
+  },
+  {
+    question:"Find margin of error",
+    detail:"A sphere is measured to have a radius of 10m with an error of ..."
+  },
+  {
+    question:"Find margin of error",
+    detail:"A sphere is measured to have a radius of 10m with an error of ..."
+  }
+];
+
+const exam_problem = () => {
+    const router =useRouter()
+    const { course , exam } = router.query
+    const [problems, setProblems] = useState([{question:"",detail:""}]);
+
+  function getProblem() {
+    // const res = await fetch('...');
+    // const resJson = await res.json();
+    setProblems(problem);
+  }
+
+  useEffect(() => {
+    getProblem();
+  }, []);
+
+  console.log(problem)
+  const [count,setCount] = useState(0);
+  const postElements = problems.map(problem => {
+    return (
+      <Link
+        href={{
+          pathname: "/browse/[course]/[exam]/[problem]",
+          query: { course:`${course}` , exam:`${exam}` , problem:`${problem.question}`},
+        }}
+      >
+        <div  className="relative w-4/5 overflow-hidden border-black border rounded-2xl mb-5 hover:cursor-pointer" >
+          <div className="px-6 py-4 ">
+            <h1 className="font-bold text-xl mb-2">
+              {problem.question}
+            </h1>
+            <p className="text-gray-700 text-sm text-base mb-[45px]">
+              {problem.detail}
+            </p>
+          </div>
+          <div className="absolute bottom-[11px] right-[98px] hover:cursor-pointer" onClick={()=>setCount(count+1)} >
+            <Image src="/graphic/arrow-up.svg" width="24" height="24"/>
+          </div>
+          <div className="absolute bottom-[17px] right-[64px] hover:cursor-pointer">
+            {count}
+          </div>
+          <div className="absolute bottom-[11px] right-[13px] hover:cursor-pointer" onClick={()=>setCount(count-1)}>
+            <Image src="/graphic/arrow-down.svg" width="24" height="24"/>
+          </div>
+        </div>
+      </Link>
+    )
+  });
+
+    return(
+        <div>
+        <div className="ml-11 mt-20 mb-8">
+            <h1 className="font-bold text-4xl">Browse</h1>
+            <p className="text-2xl">{course} {exam}</p>
+        </div>
+        <div className="flex flex-wrap justify-center">
+           {postElements}
+        </div>
+        <Footer />
+        </div>
+    )
+}
+
+export default exam_problem
---
+++ src/pages/browse/index.tsx
@@ -0,0 +1,74 @@
+import Footer from "@components/Footer"
+import React, { useEffect, useState } from "react"
+import Header from "@components/Header"
+import { useRouter } from "next/router"
+
+const subject = [
+  {
+    id:"2301107",
+    name:"calculus 1",
+    problems: "90"
+  },
+  {
+    id:"2301107",
+    name:"calculus 1",
+    problems: "90"
+  },
+  {
+    id:"2301107",
+    name:"calculus 1",
+    problems: "90"
+  },
+  {
+    id:"2301107",
+    name:"calculus 1",
+    problems: "90"
+  }
+];
+
+const courses = () => {
+  const router = useRouter()
+  const [subjects, setSubjects] = useState([{id:"",name:"",problems:""}]);
+
+  function getSubject() {
+    // const res = await fetch('...');
+    // const resJson = await res.json();
+    setSubjects(subject);
+  }
+
+  useEffect(() => {
+    getSubject();
+  }, []);
+
+  console.log(subject)
+
+  const postElements = subjects.map(subject => {
+    return (
+      <div key={subject.id} className="w-4/5 overflow-hidden border-black border rounded-2xl mb-5 hover:cursor-pointer" onClick={()=>router.push(`browse/${subject.id} ${subject.name}`)}>
+        <div className="px-6 py-4">
+          <h1 className="font-bold text-xl mb-2">
+            {subject.id} {subject.name.toUpperCase()}
+          </h1>
+          <p className="text-gray-700 text-sm text-base">
+            {subject.problems} Problems
+          </p>
+        </div>
+      </div>
+    )
+  });
+
+  return (
+    <div>
+      <div className="ml-11 mt-20 mb-8">
+        <h1 className="font-bold text-4xl">Browse</h1>
+        <p className="text-2xl">Courses</p>
+      </div>
+      <div className="flex flex-wrap justify-center">
+        {postElements}
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default courses
